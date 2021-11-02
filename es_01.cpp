6003878ce1184494333f126f81945a77989180f1
Jonathan Bonetti <jb5nnhy@gmail.com>
2021-11-02 17:53:28 +0100

    Esercizio 01

--- es_01.cpp
+++ es_01.cpp
@@ -1,5 +1,16 @@
 void es_01()
 {
+    TCanvas * c = new TCanvas("c","c",600,600);
     TF1 * f1 = new TF1("f1" , "TMath::Gaus(x,[0],[1])" , 0 , 1000);
-    gauss->SetParameters()
+    double mu,sigma;
+        cout<<"Media: "<<endl;
+        cin>>mu;
+        cout<<"Deviazione Standard: "<<endl;
+        cin>>sigma;
+    f1->SetParameters(mu , sigma);
+    //f1->Draw();
+
+    TH1D * h1 = new TH1D("Gaussiana","Gaussiana",100,0,1000);
+    h1->FillRandom("f1" , 5000);
+    h1->Draw();
 }
