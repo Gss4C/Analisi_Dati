c74eaa695ccdb2410f40e9a59091f04a80d8cac6
Jonathan Bonetti <jb5nnhy@gmail.com>
2021-11-09 15:57:27 +0100

    Devo iniziare ciclo su tutti i file

--- exercise_likelihood/likelihood_scan.cpp
+++ exercise_likelihood/likelihood_scan.cpp
@@ -4,29 +4,25 @@ using namespace std;
 using namespace std;
 #include <vector>
 
-Double_t ext_likelihood(int s, int b, vector<Double_t> x, Double_t theta[4])
+Double_t ext_likelihood(int s, int b, vector<Double_t> x, Double_t theta[])
 {
     int N = x.size();
     Double_t y = 0;
-
-    TF1 *ps = new TF1("ps", "gaus", 0, 1000);
-    TF1 *pb = new TF1("pb", "1/([0]) * exp( -x/[0] )", 0, 1000);
-    TF1 *likelihood = new TF1("likelihood", "-2*log([0] * pb + [1] * ps)", 0, 1000);
-    //AGGIUNGERE POISSONIANA E VEDERE SLIDE
-    ps->SetParameters(theta[0], theta[1], theta[2]);
-    pb->SetParameter(0, theta[3]);
-    likelihood->SetParameters(b, s);
+    /*TF1 *ps = new TF1("ps", "gaus", 0, 1000);
+        TF1 *pb = new TF1("pb", "1/([0]) * TMath::Exp( -x/[0] )", 0, 1000);
+        ps->SetParameters(theta[0], theta[1], theta[2]);
+        pb->SetParameter(0, theta[3]);
+        TF1 *likelihood = new TF1("likelihood", "-2*TMath::Log([0] * ps + [1] * pb )", 0, 1000);*/
+    TF1 *likelihood = new TF1("likelihood", "-2*log( [0]/sqrt(2*TMath::Pi()*[5]*[5])*
+                                                TMath::Exp(-((x - [4]) * (x - [4])) / (2 *[5] *[5])) +
+                                                [1] / ([2]) * TMath::Exp(-x / [2])) ", 0, 1000);
+                      likelihood->SetParameters(s, b, theta[3], theta[0], theta[1], theta[2]);
     Double_t sum = 0;
     for (int i = 0; i < N; i++)
     {
         sum += likelihood->Eval(x.at(i));
-        //cout<<likelihood->Eval(x.at(i))<<endl;
     }
-
-    Double_t n_fac_strl, logn_approx;
-    n_fac_strl = TMath::Sqrt(2 * TMath::Pi() * N) * pow(N / TMath::E(), N);
-    logn_approx = log(sqrt(2*TMath::Pi()*N))+N*log(N/TMath::E());
-    y = +2*(s+b) + logn_approx + sum  ;
+    y = sum - 2 * TMath::Log(TMath::Poisson(N, s + b)) + 2 * N * TMath::Log(s + b);
     return y;
 }
 
@@ -46,55 +42,67 @@ vector<Double_t> fromfile(string filename)
 
 void likelihood_scan()
 {
-    vector<Double_t> x_1 = fromfile(("exercise_Likelihood_" + to_string(1)).c_str());
-    int N = x_1.size();
-    Double_t yi, y[N], tot_s[N];
-    Double_t theta[4];
-    TCanvas *c = new TCanvas();
-
-    theta[1] = 400;
-    theta[0] = 1 / (sqrt(2 * TMath::Pi() * theta[1] * theta[1]));
-    theta[2] = 70;
-    theta[3] = 200;
-
-    //I am considering the case b+s=N
-    int b = 500;
-    for(int i = 10; i < N; i++)
+    for (int j = 1; j < 5; j++)
     {
-        int s = i;
-        yi = ext_likelihood(s, b, x_1, theta);
-        y[i] = yi;
-        tot_s[i] = s;
+        vector<Double_t> x_1 = fromfile(("exercise_Likelihood_" + to_string(j)).c_str());
+        vector<Double_t> params = fromfile(("model_" +to_string(j)).c_str());
+        
 
-        cout << "Y= ";  cout << y[i];
-        cout << " -- "; cout << tot_s[i] << endl;
+
+        int N = x_1.size();
+        Double_t yi, y[N], tot_s[N];
+        Double_t theta[3];
+        
+        /*theta[1] = 400;
+        theta[0] = 1 / (sqrt(2 * TMath::Pi() * theta[1] * theta[1]));
+        theta[2] = 70;
+        theta[3] = 200;*/
+
+        //I am considering the case b+s=N
+        /*int b = 980;
+        TCanvas *c = new TCanvas();
+    for(int s = 19; s < N; s++)
+    {
+        yi = ext_likelihood(s, b, x_1, theta);
+        y[s-19] = yi;
+        tot_s[s-19] = s;
     }
-    TGraph *g1 = new TGraph(N, tot_s, y);
+    TGraph *g1 = new TGraph(N-19, tot_s, y);
         g1->SetTitle("Logaritmo della Likelihood al variare di S");
         g1->GetXaxis()->SetTitle("S");
-        g1->GetYaxis()->SetTitle("Log(L)");
-        g1->SetMarkerStyle(22);
+        g1->GetYaxis()->SetTitle("-2*Log(L)");
+        g1->SetMarkerStyle(4);
         g1->SetMarkerColor(4);
-        g1->Draw("AP");
+        g1->Draw("AP");*/
 
-    //I am considering the case b+s =/= N
-    
-    /*int s2_tot[N], b2_tot[N], zi, z[N*N];
-    for(int s2 = N*0.5; s2 < N*1.5; s2+=10)
-    {
-        for(int b2 = N*0.5; b2 < N*1.5; b2+=10){
-            zi=ext_likelihood(s2 , b2 , x_1 , theta);
-            z[N]=zi;
-            b2_tot[b2]=b2;
+        //I am considering the case b+s =/= N
+        TCanvas *c1 = new TCanvas();
+        TGraph2D *g2 = new TGraph2D();
+        //TH2D * h =new TH2D("H2", "H2", 500 , 0, 2*N , 500 ,0,2*N);
+        int i = 0;
+        for (int s2 = 0; s2 < N; s2 += 10)
+        {
+            for (int b2 = 0; b2 < N; b2 += 10)
+            {
+                if (s2 + b2 < 2.5 * N && s2 + b2 > 0.26 * N)
+                {
+                    double z_fin = ext_likelihood(s2, b2, x_1, theta);
+                    g2->SetPoint(i, s2, b2, z_fin);
+                    i++;
+                }
+                else
+                {
+                    g2->SetPoint(i, s2, b2, 20000);
+                    i++;
+                }
+            }
         }
-        s2_tot[s2]=s2;
+        g2->SetTitle("-2*log(L(s,b))");
+        g2->GetXaxis()->SetTitle("S");
+        g2->GetYaxis()->SetTitle("B");
+        g2->SetMarkerStyle(22);
+        g2->Draw("surf1");
     }
-    TGraph2D * g2 = new TGraph2D(N*N , s2_tot , b2_tot , z);
-    TCanvas *c1 = new TCanvas();
-    g2->SetTitle("Funzione bi");
-    g2->GetXaxis()->SetTitle("S");
-    g2->GetYaxis()->SetTitle("Bitch do n't kill my vibe");
-    g2->Draw();*/
 
     /*
     This was the first test, made for try functions
